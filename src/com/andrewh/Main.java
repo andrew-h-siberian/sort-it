f52513619fe9703d5cd6052cedd3a2b3011d78f5
Andrew_H <andrew_h_siberian@rambler.ru>
2017-03-03 15:20:12 +0700

    Change readAsIntegers to readAsIntegersAndSort

--- src/com/andrewh/Main.java
+++ src/com/andrewh/Main.java
@@ -2,11 +2,14 @@ package com.andrewh;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    static String readAsIntegers (File fileToReadAsIntegers, Boolean ascending) {
+
+    static List readAsIntegersAndSort(Scanner scToRead, Boolean ascending) {
         String sortOrder = null;
         if (ascending) {
             sortOrder = "ascending";
@@ -15,10 +18,23 @@ public class Main {
             sortOrder = "descending";
         }
         System.out.println("Sorting as a set of integers. Order - " + sortOrder);
-        return("Null return.");
+
+        if (!(scToRead.hasNextInt())) {
+            System.out.println("File has no integers!");
+            return(null);
+        }
+
+        List<Integer> listToSort = new ArrayList<Integer>();
+        while(scToRead.hasNext()) {
+            listToSort.add(scToRead.nextInt());
+        }
+        System.out.println("Reading done");
+        System.out.println(listToSort);
+        return(listToSort);
     }
 
-    static String readAsStrings (File fileToReadAsStrings, Boolean ascending) {
+
+    static String readAsStringsAndSort (File fileToReadAsStrings, Boolean ascending) {
         String sortOrder = null;
         if (ascending) {
             sortOrder = "ascending";
@@ -30,15 +46,19 @@ public class Main {
         return("Null return.");
     }
 
+
     static void writeResultToFile (File fileToWrite) {
         //TODO
     }
+
 
     public static void main(String[] args) {
 
         //System.out.println("Count of arguments: " + args.length);
 
+
         //args check, проверяем наличие четырех параметров CL (если не 4 - выходим)
+
         if (args.length != 4) {
             System.out.println("You should pass 4 arguments exactly, but you have passed " + args.length);
             System.out.println("Run: \"sort-it -h\" to get more information\n");
@@ -48,7 +68,9 @@ public class Main {
             System.exit(0);
         }
 
+
         //check if 1st argument is valid
+
         if (!(new File(args[0]).exists() && new File(args[0]).isFile())) {
             System.out.println("Input file that you have specified does not exist! (Specified input file is " + args[0] + ")");
             System.out.println("Run: \"sort-it -h\" to get more information\n");
@@ -57,12 +79,43 @@ public class Main {
             System.exit(0);
         }
 
-        //check if 2nd argument is valid / if specified output file already exists
-        if (new File(args[1]).exists()) {
+
+        //if 3rd argument is not valid (not "-i" or "-s") | проверяем валидность третьего аргумента
+
+        if (!(args[2].equals("-i") || args[2].equals("-s"))) {
+            System.out.println("3rd argument should be \"-i\" or \"-s\", but you have passed " + args[2]);
+            System.out.println("Run: \"sort-it -h\" to get more information\n");
+            System.out.println("3-им аргументом необходимо передать \"-i\" или \"-s\", а вы передали " + args[2]);
+            System.out.println("Запустите: \"sort-it -h\" для получения дополнительной информации");
+            System.exit(0);
+        }
+
+
+        //if 4th argument is not valid (not "-a" or "-d") | проверяем валидность четвертого аргумента
+
+        if (!(args[3].equals("-a") || args[3].equals("-d"))) {
+            System.out.println("4th argument should be \"-a\" or \"-d\", but you have passed " + args[3]);
+            System.out.println("Run: \"sort-it -h\" to get more information\n");
+            System.out.println("4-ым аргументом необходимо передать \"-a\" или \"-d\", а вы передали " + args[3]);
+            System.out.println("Запустите: \"sort-it -h\" для получения дополнительной информации");
+            System.exit(0);
+        }
+
+
+        //check if 2nd argument is valid / if specified output file (or directory) already exists
+
+        if (new File(args[1]).exists() && new File(args[1]).isDirectory()) {
+            System.out.println("Output file that you have specified can not be created, there is a directory " + args[0] + " (not a file)!");
+            System.out.println("Run: \"sort-it -h\" to get more information\n");
+            System.out.println("Указанный вами выходной файл не может быть создан, существует такая директория " + args[0] + " (не файл)!");
+            System.out.println("Запустите: \"sort-it -h\" для получения дополнительной информации");
+            System.exit(0);
+        }
+        if (new File(args[1]).exists() && new File(args[1]).isFile()) {
             System.out.println("Output file that you have specified does already exist! (Specified output file is " + args[0] + ")");
-            System.out.println("If you would like to overwrite it enter \"y\" (\"Y\"), enter anything else to exit without overwriting.");
+            System.out.println("If you would like to overwrite it enter \"y\" (or \"Y\"), enter anything else to exit without overwriting.");
             System.out.println("Указанный вами выходной файл уже существует! (Указанный выходной файл: " + args[0] + ")");
-            System.out.println("Если вы хотите перезаписать его введите \"y\" (\"Y\"), введите что угодно другое для выхода без перезаписи.");
+            System.out.println("Если вы хотите перезаписать его введите \"y\" (или \"Y\"), введите что угодно другое для выхода без перезаписи.");
             System.out.println("\"Y\"?");
             Scanner userInput = new Scanner(System.in);
             if (!(userInput.nextLine().equalsIgnoreCase("y"))) {
@@ -72,61 +125,29 @@ public class Main {
             }
         }
 
-        //if 3rd argument is not valid (not "-i" or "-s") | проверяем валидность третьего аргумента
-        if (!(args[2].equals("-i") || args[2].equals("-s"))) {
-            System.out.println("3rd argument should be \"-i\" or \"-s\", but you have passed " + args[2]);
-            System.out.println("Run: \"sort-it -h\" to get more information\n");
-            System.out.println("3-им аргументом необходимо передать \"-i\" или \"-s\", а вы передали " + args[2]);
-            System.out.println("Запустите: \"sort-it -h\" для получения дополнительной информации");
-            System.exit(0);
-        }
 
-        //if 4th argument is not valid (not "-a" or "-d") | проверяем валидность четвертого аргумента
-        if (!(args[3].equals("-a") || args[3].equals("-d"))) {
-            System.out.println("4th argument should be \"-a\" or \"-d\", but you have passed " + args[3]);
-            System.out.println("Run: \"sort-it -h\" to get more information\n");
-            System.out.println("4-ым аргументом необходимо передать \"-a\" или \"-d\", а вы передали " + args[3]);
-            System.out.println("Запустите: \"sort-it -h\" для получения дополнительной информации");
-            System.exit(0);
-        }
-
-        if (args[2].equals("-i")) {
-            //if (args[3])
-            //System.out.println("Sorting file as a set of integers");
-        } else if (args[2].equals("-s")) {
-            //System.out.println("Sorting file as a set of strings");
-            //sortAsStrings();
-        } else {
-            System.out.println("3rd argument should be -i or -s, but you have passed " + args[2]);
-            System.out.println("Run: \"sort-it -h\" to get more information\n");
-            System.out.println("3-им аргументом нужно передать -i или -s, а вы передали " + args[2]);
-            System.out.println("Запустите: \"sort-it -h\" для получения дополнительной информации");
-            System.exit(0);
-        }
+        //Starting to open&read(&sort) file
 
         Scanner sc = null;
-
         try {
-
             File fileToSort = new File(args[0]);
-            //File fileToSort = new File("inDir.txt");
-            //File fileToSort = new File("D:\\in.txt");
-            //File fileToSort = new File(sourceDir, inputFile)
             sc = new Scanner(fileToSort);
+            //Let's use ArrayList 'cause we will fill it from file and will not change number (quantity) of elements
+            List<Integer> resultList = new ArrayList<Integer>();
             System.out.println("File opened. (Файл открыт.)");
-
-            while (sc.hasNext()) {
-                //System.out.println(sc.next());
-                sc.next();
+            if (args[2].equals("-i")) {
+                System.out.println("Starting to sort file as a set of integers");
+                resultList = readAsIntegersAndSort(sc, args[3].equals("-a"));
+                //TODO ЗДЕСЬ ПОЛУЧАЕМ РЕЗУЛЬТАТ СОРТИРОВКИ В ВИДЕ List. Надо записать его в output file
+                System.out.println(resultList.toString());
+            } else if (args[2].equals("-s")) {
+                //System.out.println("Sorting file as a set of strings");
+                //sortAsStrings();
             }
-
         } catch (FileNotFoundException e) {
-
-            System.out.println("Error: File not found! (Ошибка: Файл не найден!)");
-            //TODO
-
+            System.out.println("Error: problems opening input file ! (Ошибка: проблемы при открытии входного файла!)");
         } finally {
-            sc.close();
+        sc.close();
         }
     }
 }
